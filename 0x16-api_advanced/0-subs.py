7f355c831a11d3094f5f8f8ab92985933cc63002
1stjanjona <jannat.hammouche@outlook.com>
2024-06-05 13:31:36 -0700

    devops

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,5 +12,10 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    if response.status_code != 200:
+        return 0
+    try:
+        results = response.json().get("data")
+        return results.get("subscribers", 0)
+    except ValueError:
+        return 0
